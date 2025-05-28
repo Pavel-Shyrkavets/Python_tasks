7b1107c52ea5cc0310a3bbd6d8b9bc0032b133f9
Pavel Shyrkavets <pshyrkavets@gmail.com>
2025-05-28 06:27:06 +0200

    Edited time_decorator.py

--- tasks/time_decorator.py
+++ tasks/time_decorator.py
@@ -1,10 +1,10 @@
 from time import sleep
 from typing import Dict
-import datetime
+from datetime import datetime
 
 execution_time: Dict[str, float] = {}
 
-def time_decorator(fn):
+def time_decorator(func):
     """
     Create a decorator function `time_decorator`
     which has to calculate decorated function execution time
@@ -12,12 +12,21 @@ def time_decorator(fn):
     decorated function name and `value` is this function execution time.
     """
 
-    def wrapper(a, b, sleep_time):
-        start = datetime.datetime.now()
-        sleep(sleep_time)
-        total = fn(a, b, sleep_time)
-        end = datetime.datetime.now()
+    def wrapper(a, b):
+        start = datetime.now()
+        total = func(a, b)
+        end = datetime.now()
         diff = end - start
-        execution_time[fn.__name__] = float(diff.total_seconds())
+        execution_time[func.__name__] = float(diff.total_seconds())
         return total
     return wrapper
+
+@time_decorator
+def func_add(a, b):
+    sleep(0.2)
+    return a + b
+
+#x, y = 10, 20
+#result = func_add(x, y)
+#print(f"{x} + {y} = {result}")
+#print(f"Execution time: {execution_time['func_add']} seconds")
